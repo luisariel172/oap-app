ba66a0a2cc14758bfd2fccb7992ff76ae73743ec
Ariel <sitioslaa@gmail.com>
2022-09-21 15:35:20 -0300

    Creacion oap-app1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <AvatarJsx /> 
+        <AvatarJsx />
         <h1>BIENVENIDOS</h1>
         <button>boton</button>
         <input/>
